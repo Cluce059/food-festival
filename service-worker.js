1f2bd843a6db33a7f2d526acfc58cfdd30c654df
caroline <cluce059@gmail.com>
2021-10-20 11:35:41 -0400

    offline functionality

--- service-worker.js
+++ service-worker.js
@@ -23,3 +23,40 @@ self.addEventListener('install', function (e) {
       })
     )
   })
+
+  self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+
+        return Promise.all(
+          keyList.map(function(key, i){
+            if(cacheKeeplist.indexOf(key) === -1){
+              console.log('deleting cache :' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+    });
+
+
+    //listen for fetch event, log url of req
+  self.addEventListener('fetch', function(e){
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function(request){
+        if(request){ //if cache vailble, res with cache
+          console.log('responding with cache: '+ e.request.url)
+          return request
+        } else { // no cache, try fetching req
+          console.log('file id not caches, fetching : '+ e.request.url)
+          return fetch(e.request)
+        }
+      })
+    );
+  });
